3b4165eec1b311eba068300cb9596cedfacbd6c3
S Krish <krishs27901@gmail.com>
2025-06-14 14:57:13 +0000

    Error: Invalid src prop (https://zenprospect-production.s3.amazonaws.com

--- next.config.ts
+++ next.config.ts
@@ -41,6 +41,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'zenprospect-production.s3.amazonaws.com', // Added S3 hostname
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
